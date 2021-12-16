919d673b9d0383b31920b031ae9d3b2ba8dd8a55
Jack andy <80754943+Jacksito98@users.noreply.github.com>
2021-12-16 02:19:20 -0400

    Add files via upload

---
+++ functions/menu.js
@@ -0,0 +1,165 @@
+exports.menu = (v) => {
+  return `✘⃝🥷 *LISTA* 
+° 式 ⃟🎈 ${v}menuvc
+° 式 ⃟🎈 ${v}bot <text>
+° 式 ⃟🎈 ${v}info
+° 式 ⃟🎈 ${v}language
+° 式 ⃟🎈 ${v}ping
+° 式 ⃟🎈 ${v}owner
+° 式 ⃟🎈 ${v}join <link>
+° 式 ⃟🎈 ${v}report <text>
+° 式 ⃟🎈 ${v}listprem
+° 式 ⃟🎈 ${v}listban
+° 式 ⃟🎈 ${v}chatbot on/off
+° 式 ⃟🎈 ${v}voicecmd on/off
+
+_*<LIST MENU + />*_
+
+   ✘⃝🦈 *FUN* 
+° 式 ⃟🥷 ${v}fake text|@user|text
+° 式 ⃟🥷 ${v}preg
+
+° 式 ⃟🌹 ${v}verdad
+° 式 ⃟🌹 ${v}reto
+° 式 ⃟🌹 ${v}ppt
+
+   _*<STICKER + />*_
+
+° 式 ⃟🧾 ${v}sticker <nombre>|<autor>
+° 式 ⃟🧾 ${v}take <nombre>|<autor>
+° 式 ⃟🧾 ${v}toimg
+° 式 ⃟🧾 ${v}attp
+
+   _*<PINTURE + />*_
+
+° 式 ⃟🌹 ${v}pinterest
+° 式 ⃟🌹 ${v}imagen
+° 式 ⃟🌹 ${v}girl
+° 式 ⃟🌹 ${v}man
+° 式 ⃟🌹 ${v}wallpaper
+ 
+   _*<TOOLS + />*_
+
+° 式 ⃟🌹 ${v}afk <razon>
+° 式 ⃟🌹 ${v}fakethumb
+° 式 ⃟🌹 ${v}tomp3
+° 式 ⃟🌹 ${v}toav
+° 式 ⃟🌹 ${v}q
+° 式 ⃟🌹 ${v}say <text>
+  
+   _*<EDUCACIÓN + />*_
+
+° 式 ⃟🌹 ${v}translate <lang><text>
+
+   _*<BUSQUEDA + />*_
+   
+° 式 ⃟🌹 ${v}playstore
+° 式 ⃟🌹 ${v}ytsearch
+° 式 ⃟🌹 ${v}google
+
+   _*<DESCARGAS + />*_
+
+° 式 ⃟🌹 ${v}play
+° 式 ⃟🌹 ${v}play2
+° 式 ⃟🌹 ${v}playvid
+° 式 ⃟🌹 ${v}ytmp3 <link yt>
+° 式 ⃟🌹 ${v}ytmp4 <link yt>
+° 式 ⃟🌹 ${v}igdl <link>
+° 式 ⃟🌹 ${v}tiktok <link>
+° 式 ⃟🌹 ${v}tiktoknowm <link>
+° 式 ⃟🌹 ${v}tiktokaudio <link>
+° 式 ⃟🌹 ${v}mediafire <link>
+  
+   _*<TEXT MAKER + />*_
+
+° 式 ⃟🌹 ${v}nameninja <text>
+° 式 ⃟🌹 ${v}typewriter <text>
+° 式 ⃟🌹 ${v}blackpill <text>
+° 式 ⃟🌹 ${v}sans <text>
+° 式 ⃟🌹 ${v}castle <text>
+
+
+   _*<TEXT MAKER + />*_
+
+° 式 ⃟🌹 ${v}addimg <text>
+° 式 ⃟🌹 ${v}addvid <text>
+° 式 ⃟🌹 ${v}addstick <text>
+° 式 ⃟🌹 ${v}addav <text>
+° 式 ⃟🌹 ${v}getimg <text>
+° 式 ⃟🌹 ${v}getvid <text>
+° 式 ⃟🌹 ${v}getstick <text>
+° 式 ⃟🌹 ${v}getav <text>
+° 式 ⃟🌹 ${v}listmedia
+
+
+   _*<VOTING + />*_
+     
+° 式 ⃟🌹 ${v}voting <text>
+° 式 ⃟🌹 ${v}delvote
+° 式 ⃟🌹 ${v}checkvote
+
+
+   _*<ADMIN + />*_
+   
+° 式 ⃟🌹 ${v}setwelcome
+° 式 ⃟🌹 ${v}seybye
+° 式 ⃟🌹 ${v}delwelcome
+° 式 ⃟🌹 ${v}delbye
+° 式 ⃟🌹 ${v}simulate <welcome/bye>
+° 式 ⃟🌹 ${v}group <open/close>
+° 式 ⃟🌹 ${v}resetlink
+° 式 ⃟🌹 ${v}banchat
+° 式 ⃟🌹 ${v}unbanchat
+° 式 ⃟🌹 ${v}hidetag
+° 式 ⃟🌹 ${v}notify
+° 式 ⃟🌹 ${v}tagall
+° 式 ⃟🌹 ${v}setppgp
+° 式 ⃟🌹 ${v}setname <text>
+° 式 ⃟🌹 ${v}setdesc <text>
+° 式 ⃟🌹 ${v}add <numero>
+° 式 ⃟🌹 ${v}kick @user
+° 式 ⃟🌹 ${v}promote @user
+° 式 ⃟🌹 ${v}demote @user
+° 式 ⃟🌹 ${v}warn @user
+° 式 ⃟🌹 ${v}delwarn @user
+° 式 ⃟🌹 ${v}warns @user
+
+
+     _*<GRUPO + />*_
+
+° 式 ⃟🌹 ${v}welcome on/off
+° 式 ⃟🌹 ${v}antidelete on/off
+° 式 ⃟🌹 ${v}detect on/off
+° 式 ⃟🌹 ${v}antilink on/off
+° 式 ⃟🌹 ${v}link
+° 式 ⃟🌹 ${v}infogp
+° 式 ⃟🌹 ${v}profile
+° 式 ⃟🌹 ${v}invite <549xxxx> 
+
+   _*<OWNER + />*_
+     
+° 式 ⃟🌹 ${v}addpremium @user
+° 式 ⃟🌹 ${v}delpremium @user
+° 式 ⃟🌹 ${v}ban @user
+° 式 ⃟🌹 ${v}unban @user
+° 式 ⃟🌹 ${v}view
+° 式 ⃟🌹 ${v}grouplist
+° 式 ⃟🌹 ${v}update
+° 式 ⃟🌹 ${v}setppbot
+° 式 ⃟🌹 ${v}setfakethumb
+° 式 ⃟🌹 ${v}setprefix
+° 式 ⃟🌹 ${v}listreport
+° 式 ⃟🌹 $
+° 式 ⃟🌹 >`;
+};
+
+exports.menuVC = `
+┌─⊷ *MAIN* 
+式 ⃟🌹 menu
+└─────────────
+┌─⊷ *BUSQUEDA*
+° 式 ⃟🌹 google
+└─────────────
+┌─⊷ *DOWNLOAD* 
+° 式 ⃟🌹 play
+└─────────────`;
